2630052382aa28cf986310b97f97fa8bbb7527e1
Martín Stanicio <martuytimo@gmail.com>
2024-09-15 19:34:56 -0300

    add warehouse size config

---
+++ src/config.py
@@ -0,0 +1,10 @@
+min_row = 50
+max_row = 53
+min_column = 1
+max_column = 90
+min_level = 1
+max_level = 5
+
+rows = [i for i in range(min_row, max_row + 1)]
+columns = [i for i in range(min_column, max_column + 1)]
+levels = [i for i in range(min_level, max_level + 1)]
