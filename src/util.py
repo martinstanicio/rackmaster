89e4ea53c6d7a711b18211683a91e4011e8727c2
Martín Stanicio <martuytimo@gmail.com>
2024-09-15 20:15:21 -0300

    add docs to util functions

--- src/util.py
+++ src/util.py
@@ -1,7 +1,10 @@
 from config import rows, columns, levels
 
 
-def is_valid_coordinate(xx: int, yyy: int, zz: int) -> bool:
+def are_valid_coordinates(xx: int, yyy: int, zz: int) -> bool:
+    """
+    Returns `True` if the coordinates are within the boundaries configured, or `False` otherwise.
+    """
     if xx not in rows or yyy not in columns or zz not in levels:
         return False
 
@@ -9,6 +12,12 @@ def is_valid_coordinate(xx: int, yyy: int, zz: int) -> bool:
 
 
 def get_pallet_origin(yyy: int) -> int:
+    """
+    Returns the `yyy` coordinate for the first slot in the pallet the given slot is in.
+
+    Each rack can hold 3 pallets, which are themselves divided into 3 slots, resulting in 9 available slots per rack.
+    Given the column `yyy`, the function returns the number of the first column in the corresponding pallet.
+    """
     while True:
         if yyy % 3 == 1:
             return yyy
