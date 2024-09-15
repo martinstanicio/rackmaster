c3bbda03dfbc4006ecf6ef0d8e180eb9e4716b64
Martín Stanicio <martuytimo@gmail.com>
2024-09-15 17:33:59 -0300

    create Slot class

---
+++ src/slot.py
@@ -0,0 +1,27 @@
+class Slot:
+    def __init__(
+        self,
+        xx: int,
+        yyy: int,
+        zz: int,
+        article_code: str,
+        quantity: int = 0,
+        status: str = "divided_pallet",
+    ) -> None:
+        if quantity < 0:
+            raise Exception("`quantity` must be a positive number")
+
+        match status:
+            case "blocked" | "full_pallet" | "divided_pallet":
+                pass
+            case _:
+                raise Exception(
+                    "`status` must be 'blocked', 'full_pallet', or 'divided_pallet'"
+                )
+
+        self.xx = xx
+        self.yyy = yyy
+        self.zz = zz
+        self.article_code = article_code
+        self.quantity = quantity
+        self.status = status
