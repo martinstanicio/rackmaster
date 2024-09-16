a166b878b57b2a0d7434e78ec140568b89ba3878
Martín Stanicio <martuytimo@gmail.com>
2024-09-16 01:20:44 -0300

    add get_free_slots function

---
+++ src/inbound.py
@@ -0,0 +1,12 @@
+from sqlalchemy.orm import Session
+
+from slot import Slot
+from status import Status
+
+
+def get_free_slots(session: Session) -> list[Slot]:
+    return (
+        session.query(Slot)
+        .where(Slot.status != Status.blocked and Slot.quantity == 0)
+        .all()
+    )
