be9c72b79caa79d4c252bde7f8b971b521087e3b
Martín Stanicio <martuytimo@gmail.com>
2024-09-18 23:18:00 -0300

    add missing docstrings

--- src/inbound.py
+++ src/inbound.py
@@ -6,6 +6,9 @@ from util import are_valid_coordinates, get_pallet_origin, is_int
 
 
 def get_free_slots(session: Session) -> list[Slot]:
+    """
+    Returns a list of all slots that are not blocked and have 0 items.
+    """
     return (
         session.query(Slot)
         .where(Slot.status != Status.blocked, Slot.quantity == 0)
@@ -19,6 +22,9 @@ def validate_inbound(
     yyy: int,
     zz: int,
 ) -> None:
+    """
+    Raises an exception if coordinates are not valid, or quantity is not a positive integer.
+    """
     if not are_valid_coordinates(xx, yyy, zz):
         raise Exception(
             f"Invalid coordinates: ({xx:02d}, {yyy:03d}, {zz:02d}). Coordinates are out of bounds."
@@ -36,6 +42,9 @@ def validate_inbound(
 def validate_slot_availability(
     target_slot: Slot,
 ) -> None:
+    """
+    Raises an exception if the target slot is blocked or not empty.
+    """
     xx = target_slot.xx
     yyy = target_slot.yyy
     zz = target_slot.zz
@@ -60,6 +69,10 @@ def register_inbound(
     zz: int,
     use_full_pallet: bool,
 ) -> None:
+    """
+    Stores `article_code` and `quantity` in the corresponding slot, or slots if it is a full pallet.
+    Raises an exception if there is any error, or saves the changes otherwise.
+    """
     try:
         validate_inbound(quantity, xx, yyy, zz)
 
