f6ce1298c1a809003659a05a20965df79559e757
Martín Stanicio <martuytimo@gmail.com>
2024-09-22 15:01:43 -0300

    refactor db.py as a class

--- src/db.py
+++ src/db.py
@@ -1,26 +1,45 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import Session, sessionmaker
 
 from base import Base
-from config import columns, db_url, levels, rows
 from slot import Slot
 
-engine = create_engine(db_url)
-Base.metadata.create_all(bind=engine)
 
-Session = sessionmaker(bind=engine)
-session = Session()
+class Database:
+    session: Session
 
-# if `warehouse` table is empty, it needs to be populated
-first_time = len(session.query(Slot).all()) == 0
-if first_time:
-    try:
-        for xx in rows:
-            for yyy in columns:
-                for zz in levels:
-                    session.add(Slot(xx, yyy, zz))
-    except Exception as e:
-        print(f"An error ocurred while populating the database: {e}")
-    else:
-        print("Populating `warehouse` table...")
-        session.commit()
+    def __init__(
+        self,
+        db_url: str,
+        rows: list[int],
+        columns: list[int],
+        levels: list[int],
+    ):
+        self.rows = rows
+        self.columns = columns
+        self.levels = levels
+        engine = create_engine(db_url)
+        Base.metadata.create_all(bind=engine)
+
+        _Session = sessionmaker(bind=engine)
+        self.session = _Session()
+
+        # if `warehouse` table is empty, it needs to be populated
+        first_time = len(self.session.query(Slot).all()) == 0
+        if first_time:
+            self.populate()
+
+    def populate(self):
+        try:
+            for xx in self.rows:
+                for yyy in self.columns:
+                    for zz in self.levels:
+                        self.session.add(Slot(xx, yyy, zz))
+        except Exception as e:
+            print(f"An error ocurred while populating the database: {e}")
+        else:
+            print("Populating `warehouse` table...")
+            self.session.commit()
+
+    def close(self):
+        self.session.close()
